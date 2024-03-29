ac7e576e93c984f1714ed06b9883fdfbe5229852
arrister1 <nurultyong2304>
2024-03-29 21:00:25 +0700

    fix layout

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.calculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.example.calculatorapp.databinding.ActivityMainBinding
+import com.google.android.material.card.MaterialCardView
+
+
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
+//    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//
+//        binding.cvCost1.setOnClickListener()
+//        binding.cvCost2.setOnClickListener()
+//        binding.cvCost3.setOnClickListener()
+//
+//        val view = binding.root
+//        setContentView(view)
+
+
     }
 }
+
+//private fun MaterialCardView.setOnClickListener() {
+//    TODO("Not yet implemented")
+//}
+
